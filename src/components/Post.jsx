98cf8d22294eb4ddfd6c70e834e2fb2b27a8b73f
Alysson Bormannn <alysson.bormannn@gmail.com>
2023-05-05 09:30:21 -0400

    created module post

---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/AlyssonBormann.png"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Alysson Bormann</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1H</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galera 🚨!</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+        <p>🏆{' '} <a href="#">xvideos.com</a></p>
+        <p>
+          <a href="#">#projeto</a>{' '}
+          <a href="#">#show</a>{' '}
+          <a href="#">#bola</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário"/>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
