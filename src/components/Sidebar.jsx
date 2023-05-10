caa601ca508c8233db4b535c6a9d01b76c7c356b
Alysson Bormannn <alysson.bormannn@gmail.com>
2023-05-10 10:33:51 -0400

    create module avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar(){
   return(
@@ -7,7 +8,7 @@ export function Sidebar(){
       <img className={styles.cover} src='https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80'/>
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src='https://github.com/AlyssonBormann.png'/>
+        <Avatar src="https://github.com/AlyssonBormann.png"/>
         <strong>Alysson Bormann</strong>
         <span>Web Developer</span>
       </div>
